9b620007cd08648d7948a69388125482eee721d6
Jonatan Gonzalez <3005959480101@ingenieria.usac.edu.gt>
2023-03-13 19:22:22 -0600

    update fix

---
+++ docs/assets/js/cargaMasiva.mjs
@@ -0,0 +1,71 @@
+import AVL from "./arbolAVL.mjs";
+import Usuario from "./usuario.mjs";
+
+document.addEventListener("DOMContentLoaded", function () {
+  function cargarArchivo() {
+    const inputCargaMasiva = document.getElementById("InputFileCargaMasiva");
+
+    /* leer json usuarios.csv  sintaxins {
+	"alumnos": [
+		{
+			"nombre": "Ebany",
+			"carnet": "201403877",
+			"password": "qwerty12",
+			"CarpetaRaiz": "/"
+		},
+		{
+			"nombre": "Bryan",
+			"carnet": "201700375",
+			"password": "Bryan131",
+			"CarpetaRaiz": "/"
+		}
+	]
+} */
+    const file = inputCargaMasiva.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function () {
+      const usuarios = JSON.parse(reader.result);
+      console.log(usuarios);
+      //variable para guardar el arbol
+      let arbolAVL;
+
+      /* verificar si ya existe arbol en localstorage */
+      if (localStorage.getItem("arbolAVL") === null) {
+        //si no existe, crear el arbol
+        arbolAVL = new AVL();
+      } else {
+        //si existe, cargar el arbol
+        arbolAVL = new AVL();
+        arbolAVL.fromJSON(localStorage.getItem("arbolAVL"));
+      }
+
+      //recorrer el json y agregar los usuarios
+      usuarios.alumnos.forEach((usuario) => {
+        const nuevoUsuario = new Usuario(
+          usuario.nombre,
+          usuario.carnet,
+          usuario.password,
+          usuario.CarpetaRaiz
+        );
+        //agregar el usuario al arbol
+        arbolAVL.insertar(nuevoUsuario);
+      });
+      //guardar o actualizar el arbol en localstorage
+      localStorage.setItem("arbolAVL", arbolAVL.toJSON());
+      //limpiar el input file
+      inputCargaMasiva.value = "";
+      
+      //mostrar mensaje de exito
+      alert("Carga masiva exitosa");
+      console.log("LOCAL STORAGE: ", localStorage.getItem("arbolAVL"));
+      
+    };
+  }
+
+  //agregar listener al boton de subir archivo
+  document.getElementById("SubirCargaMasiva").addEventListener("click", () => {
+    cargarArchivo();
+    console.log("subiendo archivo");
+  });
+});
